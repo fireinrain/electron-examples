8d85afad3f6154766bc27d6fe4669e8ba6d74d2b
lzhaoyang <575563079@qq.com>
2018-11-27 23:50:02 +0800

    shell module

---
+++ 9-shell-module/index.js
@@ -0,0 +1,21 @@
+
+
+const fileBtn = document.getElementById("openFile");
+const floderBtn = document.getElementById("openFolder");
+const linkBtn = document.getElementById("openLink");
+
+const shell = require("electron").shell
+
+fileBtn.addEventListener("click",function () {
+    shell.showItemInFolder("E:\\pdf书籍\\think-in-java.pdf");
+});
+
+floderBtn.addEventListener("click",function(){
+    shell.openItem("E:\\pdf书籍\\think-in-java.pdf");
+});
+
+linkBtn.addEventListener("click",function(){
+    shell.openExternal("https:www.youtube.com");
+})
+
+
---
+++ 9-shell-module/main.js
@@ -0,0 +1,42 @@
+"use strict"
+
+
+console.log("main process working");
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow();
+    win.loadURL(url.format({
+        pathname:path.join(__dirname,'index.html'),
+        protocol:'file',
+        slashes:true
+    }));
+
+    //开启devtools
+
+    //win.webContents.openDevTools();
+
+    win.on("close",()=>{
+        win = null;
+    });
+}
+
+app.on("ready",createWindow);
+app.on("window-all-closed",()=>{
+    if (process.platform!== "darwin") {
+        app.quit()
+    }
+});
+app.on("activate",()=>{
+    if (win===null) {
+        createWindow()
+    }
+});
